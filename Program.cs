397509e11d26958dbcaa75620344f59edb883e4b
hossamgamal-1 <hossamgmal1313@gmail.com>
2025-04-25 20:39:01 +0300

    - Added DbContext With Identity.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TodoAppApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//configure Identity
+builder.Services.AddIdentity<AppUser,IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+// Add connection string to the database
+string connectionStr = builder.Configuration.GetConnectionString("DefaultConnection")!;
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionStr));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
